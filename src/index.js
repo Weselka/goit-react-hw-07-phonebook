a26d34c2333522caabeab5cb957872048270af77
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2023-01-25 16:09:42 +0200

    clean redux-persist

--- src/index.js
+++ src/index.js
@@ -2,25 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
 import { Global, ThemeProvider } from '@emotion/react';
-
 import { GlobalStyles, theme } from 'styles';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components';
-import {
-  store,
-  // persistor
-} from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-        <ThemeProvider theme={theme}>
-          <Global styles={GlobalStyles} />
-          <App />
-        </ThemeProvider>
-      {/* </PersistGate> */}
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
