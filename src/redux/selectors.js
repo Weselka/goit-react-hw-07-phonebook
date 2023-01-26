1476936bd9ef58ef3270b9f49b04bd95b5700027
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2023-01-26 12:22:40 +0200

    changes selectVisibleContacts

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectIsLoading = state => state.contacts.isLoading;
@@ -6,11 +8,12 @@ export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state);
-  const normalized = filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalized)
-  );
-};
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalized = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalized)
+    );
+  }
+);
